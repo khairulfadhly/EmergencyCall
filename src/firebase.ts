e99abf44e51e5dcbe4bfc113b20eb2f11e374053
khairul fadhly <54829667+khairulfadhly@users.noreply.github.com>
2024-01-31 17:05:38 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,45 +1,21 @@
-// INI KODE ORIGINAL!!!
-
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-
-// const firebaseConfig = {
-  // apiKey: "AIzaSyCbpPaEwoC-X-QFPIFHNoaimo95baQs-UE",
-  // authDomain: "emergencyapps-baf36.firebaseapp.com",
-  // projectId: "emergencyapps-baf36",
-  // storageBucket: "emergencyapps-baf36.appspot.com",
-  // messagingSenderId: "735152341016",
-  // appId: "1:735152341016:web:ba561e8b05659c462a836d"
-
-// };
-
-// const app = firebase.initializeApp(firebaseConfig);
-
-
-
-// export const auth = app.auth();
-
-
-// export { firebase };
-
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/firestore'; // Tambahkan ini untuk mengimpor modul firestore
+import 'firebase/firestore'; 
 
 const firebaseConfig = {
  
-  apiKey: "AIzaSyCbpPaEwoC-X-QFPIFHNoaimo95baQs-UE",
-  authDomain: "emergencyapps-baf36.firebaseapp.com",
-  projectId: "emergencyapps-baf36",
-  storageBucket: "emergencyapps-baf36.appspot.com",
-  messagingSenderId: "735152341016",
-  appId: "1:735152341016:web:ba561e8b05659c462a836d"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_API_KEY",
+  projectId: "YOUR_API_KEY",
+  storageBucket: "YOUR_API_KEY",
+  messagingSenderId: "YOUR_API_KEY",
+  appId: "YOUR_API_KEY"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const firestore = firebase.firestore(); // Inisialisasi instance firestore
+const firestore = firebase.firestore();
 
 export { firebase, auth, firestore };
